43937249169b9ee0d4f9fc42c1a63d85f30688ef
chkp-yossefk <yossefk@checkpoint.com>
2025-09-09 11:38:50 +0300

    ifx

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ echo "Installing the application..." > /tmp/install.log
 # Simulate installation steps
 sleep 2
 echo "Installation complete!" >> /tmp/install.log
+yum install -y tcl-8.6.8-2.el8.x86_64 && yum remove tcl-8.6.8-2.el8.i686 -y
 echo "Installation complete!"
