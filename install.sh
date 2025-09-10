e2b4f671006739c8d9f4c71248a5b15e45143131
chkp-yossefk <yossefk@checkpoint.com>
2025-09-10 15:48:21 +0300

    ifx

--- install.sh
+++ install.sh
@@ -6,9 +6,6 @@ if [ -f /etc/os-release ]; then
 	. /etc/os-release
 	if [[ "$ID" == "fedora" || "$ID" == "rhel" || "$ID_LIKE" == *"rhel"* ]]; then
 		yum install -y tcl-8.6.8-2.el8.x86_64 && yum remove tcl-8.6.8-2.el8.i686 -y
-		yum install -y nginx
-	else
-		apt install -y nginx
 	fi
 else
 	echo "Skipping yum commands: OS information not found." >> /tmp/install.log
